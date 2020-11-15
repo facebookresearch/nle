5cf096f41334110b2bc4eb66de64672570049e01
Eric Hambro <ehambro@fb.com>
2020-11-15 15:31:04 +0000

    Placate black and circle ci.

--- nle/tests/test_profile.py
+++ nle/tests/test_profile.py
@@ -10,23 +10,26 @@ import gym
 import numpy as np
 
 
-BASE_KEYS = ['glyphs', 'message', 'blstats']
-MAPPED_GLYPH = ['chars', 'colors', 'specials']
-INV_GLYPH = ['inv_glyphs', 'inv_strs', 'inv_letters', 'inv_oclasses']
-SCREEN_DESC = ['screen_descriptions']
-TTY = ['tty_chars', 'tty_colors', 'tty_cursor']
+BASE_KEYS = ["glyphs", "message", "blstats"]
+MAPPED_GLYPH = ["chars", "colors", "specials"]
+INV_GLYPH = ["inv_glyphs", "inv_strs", "inv_letters", "inv_oclasses"]
+SCREEN_DESC = ["screen_descriptions"]
+TTY = ["tty_chars", "tty_colors", "tty_cursor"]
 
-EXPERIMENTS= {
-    '(1): glyphs/msg/blstats': BASE_KEYS,
-    '(2): (1)... + char/col/spec': BASE_KEYS + MAPPED_GLYPH,
-    '(3): (2)... + inv_*': BASE_KEYS + MAPPED_GLYPH + INV_GLYPH,
-    '(4): (3)... + screen_desc':BASE_KEYS + MAPPED_GLYPH + INV_GLYPH + SCREEN_DESC
+EXPERIMENTS = {
+    "(1): glyphs/msg/blstats": BASE_KEYS,
+    "(2): (1)... + char/col/spec": BASE_KEYS + MAPPED_GLYPH,
+    "(3): (2)... + inv_*": BASE_KEYS + MAPPED_GLYPH + INV_GLYPH,
+    "(4): (3)... + screen_desc": BASE_KEYS + MAPPED_GLYPH + INV_GLYPH + SCREEN_DESC,
 }
 
-@pytest.mark.parametrize('observation_keys', EXPERIMENTS.values(), ids=EXPERIMENTS.keys())
+
+@pytest.mark.parametrize(
+    "observation_keys", EXPERIMENTS.values(), ids=EXPERIMENTS.keys()
+)
 @pytest.mark.benchmark(disable_gc=True, warmup=False)
-def test_run_1k_steps( observation_keys, benchmark):
-    env = nle.env.base.NLE(observation_keys=observation_keys)
+def test_run_1k_steps(observation_keys, benchmark):
+    env = nle.env.base.NLE(savedir=None, observation_keys=observation_keys)
     seeds = [123456]
     steps = 1000
 
@@ -36,7 +39,6 @@ def test_run_1k_steps( observation_keys, benchmark):
     def seed():
         seeds[0] += 1
         env.seed(seeds[0], 2 * seeds[0])
-
 
     def play_1k_steps():
         env.reset()
