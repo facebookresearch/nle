1f49cf0b95475a3c64488e18340f76a617f8c683
Tim Rocktäschel <rockt@users.noreply.github.com>
2021-02-19 11:50:07 +0000

    Update nle/nethack/__init__.py
    Co-authored-by: Eric Hammy <6815729+condnsdmatters@users.noreply.github.com>

--- nle/nethack/__init__.py
+++ nle/nethack/__init__.py
@@ -1,6 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 from nle.nethack.actions import *  # noqa: F403
-
 from nle._pynethack.nethack import *  # noqa: F403
 from nle.nethack.nethack import (
     Nethack,
