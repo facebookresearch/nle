395841aee429acd5d0a6094f7a31f59d808d813d
Heinrich Kuttler <hnr@fb.com>
2022-01-26 20:22:15 +0000

    Fix play.py following underscore access change. (#308)

--- nle/scripts/play.py
+++ nle/scripts/play.py
@@ -60,7 +60,7 @@ def get_action(env, is_raw_env):
                 if is_raw_env:
                     action = ch
                 else:
-                    action = env._actions.index(ch)
+                    action = env.actions.index(ch)
                 break
             except ValueError:
                 print(
@@ -113,7 +113,7 @@ def play():
             if not is_raw_env:
                 print("-" * 8 + " " * 71)
                 print(f"Previous reward: {str(reward):64s}")
-                act_str = repr(env._actions[action]) if action is not None else ""
+                act_str = repr(env.actions[action]) if action is not None else ""
                 print(f"Previous action: {str(act_str):64s}")
                 print("-" * 8)
                 env.render(FLAGS.render_mode)
